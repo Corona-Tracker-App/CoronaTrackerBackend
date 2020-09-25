4c09592bfeba4c3a29b605ed273c22a478a405b3
Böhm Jan <jan.boehm@rku-it.de>
2020-09-25 13:10:20 +0200

    Konfiguration injizieren

--- CoronaTrackerBackend.Server/Startup.cs
+++ CoronaTrackerBackend.Server/Startup.cs
@@ -14,7 +14,12 @@ namespace CoronaTrackerBackend.Server
 {
     public class Startup
     {
-        public IConfiguration Configuration { get; }
+        private IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
